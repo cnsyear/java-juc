5d46bfb6f8430aa927ce9697d08aa2c17dc424a3
KongGaoDeNiao <820382480@qq.com>
2018-10-26 13:46:20 +0800

    init

---
+++ src/main/java/com/cnsyear/JavaJucApplication.java
@@ -0,0 +1,12 @@
+package com.cnsyear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJucApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJucApplication.class, args);
+	}
+}
